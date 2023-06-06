d97b654bb1e52aaabc6051123a1c31fd5847e22b
Fernando Fernández <ferferga@hotmail.com>
2023-06-06 19:27:42 +0000

    draft: implement vuetify locale changing

---
+++ frontend/scripts/paths.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-nodejs-modules */
+import { resolve } from 'node:path';
+/* eslint-enable import/no-nodejs-modules */
+
+export const localeFilesFolder = resolve('locales/**');
+export const srcRoot = `${resolve('src')}/`;
